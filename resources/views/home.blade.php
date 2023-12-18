1a293cd024e2ab63c40be3e5b2af802d8e68b397
giovannimegliola <giovanni.megliola@hotmail.it>
2023-12-18 15:51:15 +0100

    modified cards style

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,13 +6,14 @@
 
 <main>
     <section class="container">
-        <h1>Comics</h1>
-        <div class="row">
+        <h1 style="text-transform: uppercase;">Current series</h1>
+        <div class="row g-4 ">
             @foreach ($items as $item)
 
-            <div class="col-12 col-md-4 col-lg-3">
+            <div class="col-12 col-md-4 col-lg-2">
                 <div class="card">
                     <img src="{{ $item['thumb']}}" alt="{{ $item ['title']}}">
+                    <p >{{ $item ['title']}}</p>
                 </div>
             </div>
             @endforeach
