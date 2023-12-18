98f23fb5d51326b7258e38021d13d4e3f2098d1c
giovannimegliola <giovanni.megliola@hotmail.it>
2023-12-18 17:06:46 +0100

    added navbar in header

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,13 +6,13 @@
 
 <main>
     <section class="container">
-        <h1 style="text-transform: uppercase;">Current series</h1>
+        <button class="btn btn-primary my-3"><h1 style="text-transform: uppercase;">Current series</h1></button>
         <div class="row g-4 ">
             @foreach ($items as $item)
 
             <div class="col-12 col-md-4 col-lg-2">
                 <div class="card">
-                    <img src="{{ $item['thumb']}}" alt="{{ $item ['title']}}">
+                    <img src="{{ $item['thumb']}}" alt="{{ $item ['title']}}" >
                     <p >{{ $item ['title']}}</p>
                 </div>
             </div>
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,1 +1,49 @@
-<div>Header <i class="fa-solid fa-home"></i></div>
+<header class="container">
+    <div class="row my-3 ">
+
+      <div class="col-auto">
+        <img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="logo-dc" class="logo">
+      </div>
+
+        <ul class="nav justify-content-center col">
+            <li class="nav-item">
+            <a class="nav-link" href="#">Character</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link" href="#">Comics</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link" href="#">Movies</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link" href="#">TV</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link" href="#">Games</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link" href="#">Collectibles</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link" href="#">Videos</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link" href="#">Fans</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link" href="#">News</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link" href="#">Shop</a>
+            </li>
+        </ul>
+
+        <div class="col-auto">
+            <form class="d-flex">
+              <input class="form-control me-2" type="search" placeholder="Search"></i>
+            </form>
+        </div>
+
+    </div>
+
+</header>
